12f53de93faab9b4733867f7793455be6bc56abf
mosheze1311 <mosheze1311@gmail.com>
2024-08-01 17:32:32 +0300

    added_index for lights

--- Light.py
+++ Light.py
@@ -1,12 +1,13 @@
 from surfaces.Object3D import Object3D
 
 class Light:
-    def __init__(self, position, color, specular_intensity, shadow_intensity, radius):
+    def __init__(self, position, color, specular_intensity, shadow_intensity, radius,index:int):
         self.position = position
         self.color = color
         self.specular_intensity = specular_intensity
         self.shadow_intensity = shadow_intensity
         self.radius = radius
+        self.index = index
 
     def transform_to_camera(self, view_matrix) -> None:
         """
