9c775296c2a69dd78f6c26d11290c890ee8a7579
mosheze1311 <mosheze1311@gmail.com>
2024-08-12 04:59:56 +0300

    added documentation added funcs save_image, display_image

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
+from PIL import Image
 import numpy as np
+import cv2
 
 ColorVector = np.ndarray
 Vector = np.ndarray
 Matrix = np.ndarray
-Image = np.ndarray
 
 X_DIRECTION = np.array([1, 0, 0])
 Y_DIRECTION = np.array([0, 1, 0])
@@ -50,14 +51,60 @@ def normalize_to_color(matrix):
 
 
 def diagonalize_vectors(vector1: Vector, vector2: Vector):
-    # normalize the first vector
-    vector1 = vector1 / np.linalg.norm(vector1)
+    """
+    Diagonalizes two vectors to create an orthonormal basis. The first vector is assumed to be normalized. The second
+    vector is orthogonalized with respect to the first vector, and the third vector is computed as the cross product of
+    the first two vectors.
 
+    :param vector1: ndarray of shape (..., 3) representing the first vector, which is assumed to be normalized.
+    :param vector2: ndarray of shape (..., 3) representing the second vector to be orthogonalized with respect to vector1.
+    :return: Tuple containing:
+        - vector1: The input first vector
+        - vector2: Orthogonalized and normalized version of the second input vector.
+        - vector3: Orthonormal vector computed as the cross product of the normalized vector1 and vector2.
+
+    @pre vector1 is normalized
+    """
     vector2_projection = np.sum(vector1 * vector2, axis=-1, keepdims=True)
     vector2 = vector2 - vector2_projection * vector1
     vector2 = vector2 / (np.linalg.norm(vector2, axis=-1, keepdims=True) + EPSILON)
 
     vector3 = np.cross(vector2, vector1)
-    vector3 = vector3 / np.linalg.norm(vector3)
+    vector3 = vector3 / (np.linalg.norm(vector3, axis=-1, keepdims=True)+EPSILON)
 
     return vector1, vector2, vector3
+
+
+def save_image(image_array: np.ndarray, path: str, height: int, width: int) -> None:
+    """
+    Saves an image represented as a NumPy array to the specified file path after reshaping
+    and converting it to an 8-bit unsigned integer format.
+
+    :param image_array: A NumPy array containing the image data. The array should contain values in the range [0, 1].
+    :param path: The path where the image will be saved. Should include the file name and extension (e.g., "image.png").
+    :param height: The height of the image in pixels.
+    :param width: The width of the image in pixels.
+    :return: None
+
+    @pre: image_array.size == height * width * 3
+    @pre: image_array values are normalized within the range [0, 1]
+    @post: The image is saved at the specified path in the format determined by the file extension.
+    """
+
+    # Reshape the flattened image array to the specified height, width, and 3 color channels (RGB)
+    image_array = image_array.reshape((height, width, 3))
+
+    # Convert the image array from normalized float [0, 1] to 8-bit unsigned integer [0, 255]
+    image_array = (image_array * 255).astype(np.uint8)
+
+    # Convert the NumPy array to a PIL Image object
+    image = Image.fromarray(image_array)
+
+    # Save the image to the specified path
+    image.save(path)
+
+
+def display_image(image_colors: np.ndarray):
+    bgr_image = cv2.cvtColor(image_colors, cv2.COLOR_RGB2BGR)
+    cv2.imshow('RGB Image', bgr_image)
+    cv2.waitKey(0)
